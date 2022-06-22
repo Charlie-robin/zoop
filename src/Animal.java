8210b2baf75751734c72cc18abc37952fb6aa294
charlie-robin <charlie.robin.richardson@gmail.com>
2022-06-22 12:23:20 +0100

    (feat): Removing lesson comments

--- src/Animal.java
+++ src/Animal.java
@@ -15,9 +15,6 @@
  */
 
 public class Animal {
-    // ADD FIELDS TO A OBJECT
-    // ACCESS-MODIFIER TYPE FIELD-NAME
-    // PRIVATE -> CAN'T BE ACCESSED / MODIFIED
     private String name;
     private String type;
 
@@ -27,24 +24,15 @@ public class Animal {
 
     private int happiness;
 
-    // CONSTRUCTOR
-    // - TAKES VALUES AND ASSIGNS THEM TO FIELDS -> BUILDING THE OBJECT
-    // - WHEN INITIALISED IT RUNS
 
     public Animal(String name, String type, String id){
-        // this -> REFERS TO THE OBJECT
         this.name = name;
         this.type = type;
         this.id = id;
-        this.happiness = (int) (Math.random() * 100 + 1); // CASTING
-        // SET TO THE COMPARISON BELOW BY DEFAULT
+        this.happiness = (int) (Math.random() * 100 + 1);
         this.isTame = this.happiness > 50;
-
     }
 
-    // GETTER -> METHOD
-    // - TO GET PRIVATE INFORMATION
-    // - UNABLE TO BE CHANGED
     public String getId() {
         return id;
     }
@@ -53,10 +41,7 @@ public class Animal {
         return isTame;
     }
 
-    //  SETTER -> METHOD
-    // - TO CHANGE PRIVATE INFORMATION
     public void setIsTame(boolean tame){
-        // CHANGE FIELD TO THE VALUE THAT GETS PASSED IN
         this.isTame = tame;
     }
 
@@ -64,15 +49,12 @@ public class Animal {
         return happiness;
     }
 
-//     HAPPINESS CANNOT GO ABOVE 100
-//     IF HAPPINESS GOES ABOVE 50 THE ANIMAL IS TAME
     public void setHappiness(int happiness) {
         if(happiness <= 100 && happiness > 0){
             this.happiness = happiness;
         }
 
         if(this.happiness > 50){
-//            this.isTame = true;
             isTame = true;
         }
     }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+/**
+ * CONNECTING OUR CLASSES TOGETHER
+ * - CREATE A USER FROM USER INPUT
+ * - CREATE A ANIMAL DB
+ * - USE COMMANDS TO INTERACT WITH CLASSES ^^^
+ */
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // INITIALISE THE CLASS
+        // - GIVE IT THE DATA IT NEEDS
+        Animal charlie = new Animal("Charlie", "Magpie", "animal-1");
+
+        Animal rob = new Animal("Rob", "Orca", "animal-2");
+
     }
 }
